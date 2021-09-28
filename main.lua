342ddd4c6e683700db034295bd15403a992325b4
ReactorNefg <svnoyt@gmail.com>
2021-09-28 15:21:33 +0300

    Update main.lua
    Убран старый код. Добавлены подсказки.

--- main.lua
+++ main.lua
@@ -1,35 +1,18 @@
 local component = require("component")
 local sha = require("sha2")
 inet = component.internet
--- file=io.open("MyFile","w")
-username = "Reactor"
---local config = require ("config.lua")
+username = "Reactor" -- https://wallet.duinocoin.com -- Ник кошелека Duino
 
-local fs = require("filesystem")
-
-local function getRealTime()
-    local name = os.tmpname()
-    local f = io.open(name, "w")
-    f:close()
-
-    local time = fs.lastModified(name) / 1000
-    fs.remove(name)
-    return time
-end
-
-local function round(num)
-    return num + (2 ^ 52 + 2 ^ 51) - (2 ^ 52 + 2 ^ 51)
-end
 
 while true do
-    local socket, reason = inet.connect("5.230.70.190", 6000)
+    local socket, reason = inet.connect("149.91.88.18", 6004) -- IP duino-pool - https://server.duinocoin.com/getPool
     if not socket then
         return nil, reason
     end
 
     print("Asking for a new job for user: " .. username)
-    socket.write("JOB," .. username .. ",ESP32")
-    os.sleep(0.1)
+    socket.write("JOB," .. username .. ",ESP32") -- ESP32 сложность майнинга
+    os.sleep(3)
     job = socket.read()
     tostring(job)
     newjob = job:sub(4)
@@ -42,18 +25,10 @@ while true do
     print("Job received: " .. lastBlockHash .. " " .. expectedHash .. " " .. difficulty)
 
     range = 100 * tonumber(difficulty) + 1
-    hashingStartTime = getRealTime()
     for result = 0, range do
         ducos1 = sha.sha1(lastBlockHash .. tostring(result))
         if ducos1 == expectedHash then
-            hashingStopTime = getRealTime()
-            timeDifference = hashingStopTime - hashingStartTime
-            hashrate = result / timeDifference
-            --send = tostring(result) .. "," .. tostring(round(hashrate)) .. "," ..  "LuaMiner 1.0" .. "," .. "Miner1" -- Не пашит хеш рейт
             send = tostring(result) .. "," .. "LuaMiner 1.0"
-            print(send)
-            -- file:write(lastBlockHash .. "," .. expectedHash	 .. "," .. send)
-            -- file:close()
             socket.write(send)
             respone = socket.read()
             os.sleep(0.5)
@@ -62,3 +37,5 @@ while true do
         end
     end
 end
+
+-- https://github.com/revoxhere/duino-coin/tree/useful-tools -- API duino
